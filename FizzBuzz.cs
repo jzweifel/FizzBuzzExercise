8ec7ebf473c5c1c1ca32141f310c0b76ef78fe79
Jacob Zweifel <jacob_zweifel@jhancock.com>
2019-01-22 13:58:21 -0500

    include example final implementation
    this example is from running the exercise from an actual candidate,
    not intended to serve as the 'de facto' target implementation

--- FizzBuzz.cs
+++ FizzBuzz.cs
@@ -9,16 +9,39 @@ namespace FizzBuzz {
                 [1] => ["1"], 
                 [1,2,3,4,5] => ["1","2","3","4","5"],
                 [1..10] => ["1","2","3","4","5","6","7","8","9","10"]
-            - Provided a null array, return a null array
             - for multiples of three, print "Fizz” instead of the number
                 [1,2,3] => ["1","2","Fizz"]
             - for multiples of five, print "Buzz"
                 [1,2,3,4,5] => ["1","2","Fizz","4","Buzz"]
             - For numbers which are multiples of both three and five,
                 print "FizzBuzz".
-        */
+
+
+                --------
+                7 => "Bang!"
+                3, 7 => "FizzBang!"
+                5, 7 => "BuzzBang!"
+                3,5,7 => "FizzBuzzBang!"
+         */
+
+        private static Dictionary<int, string> translations = new Dictionary<int, string> () { 
+            { 3, "Fizz" }, { 5, "Buzz" }, {7, "Bang!"}
+        };
         public static List<string> GetFizzBuzz (List<int> ints) {
-            throw new NotImplementedException ();
+            if (ints == null) return null;
+            return ints.Select (i => FizzBuzzTranslation (translations, i)).ToList ();
+        }
+
+        private static string FizzBuzzTranslation (Dictionary<int, string> translations, int input) {
+            var result = String.Empty;
+            foreach (var translation in translations) {
+                if (IsMultipleOf (input, translation.Key)) result += translation.Value;
+            }
+            return result != String.Empty ? result : input.ToString();
+        }
+
+        private static bool IsMultipleOf (int input, int multiple) {
+            return input % multiple == 0;
         }
     }
 }
--- FizzBuzzTest.cs
+++ FizzBuzzTest.cs
@@ -2,7 +2,8 @@ using System;
 using System.Collections.Generic;
 using Xunit;
 
-namespace FizzBuzz {
+namespace FizzBuzz
+{
     /*
 
     The Three Rules of TDD
@@ -22,15 +23,123 @@ namespace FizzBuzz {
         All tests should pass.
 
     */
-    public class FizzBuzzTest {
+    public class FizzBuzzTest
+    {
         [Fact]
-        public void GivenArrayOf1_ReturnsArrayWithString1 () {
-            var input = new List<int> () { 1 };
-            var expected = new List<string> () { "1" };
+        public void GivenArrayOf1_ReturnsArrayWithString1()
+        {
+            var input = new List<int>() { 1 };
+            var expected = new List<string>() { "1" };
 
-            var actual = FizzBuzz.GetFizzBuzz (input);
+            var actual = FizzBuzz.GetFizzBuzz(input);
 
-            Assert.Equal (expected, actual);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GivenNullArray_ReturnsNull()
+        {
+            var actual = FizzBuzz.GetFizzBuzz(null);
+
+            Assert.Equal(null, actual);
+        }
+
+        [Fact]
+        public void GivenArrayOfSize_NReturnsArrayOfSizeN()
+        {
+            var input = new List<int>() { 1, 2 };
+
+            Assert.Equal(input.Count, FizzBuzz.GetFizzBuzz(input).Count);
+        }
+
+        [Fact]
+        public void GivenArrayOfIntegers_ReturnArrayOfStrings()
+        {
+            var input = new List<int>() { 1, 2, 4 };
+            var expected = new List<string>() { "1", "2", "4" };
+
+            Assert.Equal(expected, FizzBuzz.GetFizzBuzz(input));
+        }
+
+        [Fact]
+        public void GivenArrayContainsMultiplesOf3_ReturnArrayContainsFizz()
+        {
+            var input = new List<int>() {1, 2, 3, 6};
+            var expected = new List<string>() { "1", "2", "Fizz", "Fizz" };
+
+            Assert.Equal(expected, FizzBuzz.GetFizzBuzz(input));
+        }
+
+        [Fact]
+        public void GivenArrayContainsMultiplesOf5_ReturnArrayContainsBuzz()
+        {
+            var input = new List<int>() {1, 5, 10, 2};
+            var expected = new List<string>() { "1", "Buzz", "Buzz", "2" };
+
+            Assert.Equal(expected, FizzBuzz.GetFizzBuzz(input));
+        }
+
+        [Fact]
+        public void GivenArrayContainsMultiplesOf3And5_ReturnArrayContainsFizzBuzz()
+        {
+            var input = new List<int>() {1, 15, 45, 2};
+            var expected = new List<string>() { "1", "FizzBuzz", "FizzBuzz", "2" };
+
+            Assert.Equal(expected, FizzBuzz.GetFizzBuzz(input));
+        }
+
+        [Fact]
+        public void GivenArrayContainsAll_ReturnArrayContainsAll()
+        {
+            var input = new List<int>() {1, 3, 5, 15};
+            var expected = new List<string>() { "1", "Fizz", "Buzz", "FizzBuzz" };
+
+            Assert.Equal(expected, FizzBuzz.GetFizzBuzz(input));
+        }
+
+        [Fact]
+        public void GivenArrayContainsNegative_ReturnArrayOfNegative()
+        {
+            var input = new List<int>() {-1};
+            var expected = new List<string>() { "-1" };
+
+            Assert.Equal(expected, FizzBuzz.GetFizzBuzz(input));
+        }
+
+        [Fact]
+        public void GivenArrayContainsMultipleOf7_ReturnArrayContainingBang()
+        {
+            var input = new List<int>() {7};
+            var expected = new List<string>() { "Bang!" };
+
+            Assert.Equal(expected, FizzBuzz.GetFizzBuzz(input));
+        }
+
+        [Fact]
+        public void GivenArrayContainsMultipleOf3And7_ReturnArrayContainingFizzBang()
+        {
+            var input = new List<int>() {21};
+            var expected = new List<string>() { "FizzBang!" };
+
+            Assert.Equal(expected, FizzBuzz.GetFizzBuzz(input));
+        }
+
+        [Fact]
+        public void GivenArrayContainsMultipleOf5And7_ReturnArrayContainingBuzzBang()
+        {
+            var input = new List<int>() {35};
+            var expected = new List<string>() { "BuzzBang!" };
+
+            Assert.Equal(expected, FizzBuzz.GetFizzBuzz(input));
+        }
+
+        [Fact]
+        public void GivenArrayContainsMultipleOf3And5And7_ReturnArrayContainingFizzBuzzBang()
+        {
+            var input = new List<int>() {105};
+            var expected = new List<string>() { "FizzBuzzBang!" };
+
+            Assert.Equal(expected, FizzBuzz.GetFizzBuzz(input));
         }
     }
 }
